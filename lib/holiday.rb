d48e90028d3c54a5bbfe33a60dfe4af260aac05d
Jorge Venegas <brangelina01@gmail.com>
2020-07-17 22:27:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,17 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+x = []
+holiday_hash.each do |season, data|
+  data.each do |holiday, value|
+    value.each do |supply|
+     if supply == "BBQ"
+        x.push(holiday)
+      end
+    end
+  end
+end	
+x
 end
 
 
